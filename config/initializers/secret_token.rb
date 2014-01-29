fc8c9ad635ab121d2bbaacd5a636f9bcc6f60bdd
Matias Hoyl <matiashoyl@gmail.com>
2014-01-29 11:35:30 -0300

    Cambios para deploy a heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fch::Application.config.secret_key_base = 'd6fca48a3881adf883f7e1709f226d2c96fac96658232f1cb7f01d2414b39e197cffa3e21cd80ab26c5210d9f6004b6e99b5778bad3a0f6622dfb8ed304a7c70'
